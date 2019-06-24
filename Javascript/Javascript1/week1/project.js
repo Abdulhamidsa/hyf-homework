c0ae49a462013981b7b3d82b8f134f7244ccfcb2
Abdulhamidsa <42851155+Abdulhamidsa@users.noreply.github.com>
2019-06-24 17:02:53 +0200

    Add files via upload

---
+++ Javascript/Javascript1/week1/project.js
@@ -0,0 +1,66 @@
+
+//Age-ify (A future age calculator)
+
+
+
+let yearOfBirth = 1996;
+let yearFuture = 2027;
+let age = yearFuture - yearOfBirth;
+console.log("I will be " +  age  + " years old in " + yearFuture)
+
+
+
+
+
+//second part (Goodboy-Oldboy)
+
+
+
+
+let dogYearOfBirth = 2010;
+let dogYearOfFuture = 2027;
+let dogYear = 10 ; 
+let shouldShowInDogYears = true;
+if(shouldShowInDogYears){
+    console.log(" Your dog will be " +  dogYear * 7  + " dog years old in " +  dogYearOfFuture)
+}
+else {
+    console.log(" your dog will be " +  dogYear + " in human years " +  dogYearOfFuture)
+}
+
+
+
+//third part (Housey pricey) "Peter" 
+
+
+let wide = 8;
+let deep = 10;
+let high = 10;
+let price = 2500000;
+let gardenSizeInm2 = 100;
+let volumeInMeters = wide * deep * high;
+
+housePrice = volumeInMeters * 2.5 * 100 + gardenSizeInm2 * 300;
+
+if (price > housePrice) {
+    console.log(housePrice + " is less than " + price)
+}
+else{
+    console.log(housePrice + " is expensive than " + price)
+}
+
+
+//third part (Housey pricey) "Juli"
+
+let wide2 = 5;
+let deep2 = 11;
+let high2 = 8;
+let price2 = 2500000;
+let gardenSizeInm22= 70;
+let volumeInMeters = wide * deep * high;
+
+housePrice = volumeInMeters * 2.5 * 100 + gardenSizeInm2 * 300;
+let text = "" ;
+for (price > housePrice) {
+text += housePrice + " is less than " + price 
+}
